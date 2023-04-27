3d57da2d26a03d1cb95c225ed732bbf97f4b9657
Jean Atacho <ing.jeanatacho@gmail.com>
2023-04-28 01:32:40 +0200

    Corregido redireccion al login

--- Trini_App_React/src/views/Aside.jsx
+++ Trini_App_React/src/views/Aside.jsx
@@ -8,7 +8,7 @@ function Aside() {
     
             <nav className="navBar_btn_home">
                 <div className="login_btn">
-                    <Link to="./login.jsx">Iniciar sesion</Link>
+                    <Link to="./login">Iniciar sesion</Link>
                 </div>
                 <div className="register_btn">
                     <Link to="http://google.com">Registrar Usuario</Link>
