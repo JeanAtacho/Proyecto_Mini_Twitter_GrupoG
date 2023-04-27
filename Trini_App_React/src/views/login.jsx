4aab52d5197858c60bfb6d169d0f95a841237327
Jean Atacho <ing.jeanatacho@gmail.com>
2023-04-27 20:37:26 +0200

    9989

--- Trini_App_React/src/views/login.jsx
+++ Trini_App_React/src/views/login.jsx
@@ -17,31 +17,24 @@ function Login() {
   return (
       <div main className="allForms">
         <form onSubmit={handleSubmit} className="form">
-            <img className="logo_trini" src="./assets/Images/Logo_Trini_redi.png" alt="Logo_Trini" />
+            <img className="logo_trini" src="src/image/Logo_Trini_redi.png" alt="Logo_Trini" />
 
             <div className="inputContainer">
+            <label htmlFor="email" className="label">Correo</label>
+            </div>
+            <div className="inputContainer">
                 <input type="text" name="email" id="email" className="input" placeholder="juan@example.com" required />
-                <label htmlFor="email" className="label">Correo</label>
+            </div>
+
+            <div className="inputContainer">
+            <label htmlFor="password" className="label">Contraseña</label>
             </div>
             <div className="inputContainer">
                 <input type="password" name="password" id="password" className="input" placeholder="123456" required />
-                <label htmlFor="password" className="label">Contraseña</label>
             </div>
-            <hr /> 
-            <input type="submit" className="submitBtn" value="Ingresar" />
 
-
-            <h3 className="btnTitle">¿Ya eres usuario <span>Trini</span>?</h3>
-
-            <h3 className="btnTitle">¿Aún no eres usuario <span>Trini</span>?</h3>
-            <input type="submit" className="submitBtn" value="Registrar" />
-
-            <h3 className="btnTitle">¿Quieres saber de que va <span>Trini</span> <br /> como anónimo?</h3>
-            <input type="submit" className="submitBtn" value="Explorar" />
+            <input type="submit" className="submitBtn" value="Iniciar Sesion" />
         </form>
-
-        <img className="logoTriniBlanco" src="./assets/Images/Logo_Trini_Blanco.png" alt="Logo_trini_blanco" />
-
     </div>
   )
 }
